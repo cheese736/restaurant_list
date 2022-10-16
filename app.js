ab43964185454b2a403d913820401ad28f413b2d
Aither <cheese736@hotmail.com>
2022-10-16 14:41:26 +0800

    refacotr: modify mongoose config

--- app.js
+++ app.js
@@ -1,24 +1,12 @@
 // require packages used in the project
 const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
-const Resto = require('./models/restaurant') 
-const routes = require('./routes')
-const restaurant = require('./models/restaurant')
 const methodOverride = require('method-override')
 
-mongoose.connect(process.env.MONGODB2_URI, {useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+const routes = require('./routes')
+require('./config/mongoose')
 
+const app = express()
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -29,11 +17,8 @@ app.use(express.static('public'))
 app.use(methodOverride('_method'))
 app.use(express.urlencoded({ extended: true }))
 
-// routes setting
-
-// 進入首頁
+// 設定路由
 app.use(routes)
-
 
 // 設定 port 3000
 app.listen(3000, () => {
--- models/seeds/seeder.js
+++ models/seeds/seeder.js
@@ -1,29 +1,26 @@
-const mongoose = require('mongoose')
+const db = require('../../config/mongoose')
 const restaurants = require('../../restaurant.json')
-const Resto = require('../restaurant') // 載入 todo model
+const Resto = require('../restaurant') // 載入 restaurant model
 
-mongoose.connect(process.env.MONGODB2_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
 
 db.once('open', () => {
-  console.log('mongodb connected!')
-  Resto.deleteMany({}).then(() => console.log('clear data'))
+  Resto.deleteMany({})
+  .then(() => console.log('clear data'))
+  .then(() => {
+    for (resto of restaurants.results) {
+      Resto.create({
+        name: resto.name,
+        name_en: resto.name_en,
+        category: resto.category,
+        image: resto.image,
+        location: resto.location,
+        phone: resto.phone,
+        google_map: resto.google_map,
+        rating: resto.rating,
+        description: resto.description
+      })
+    }
+  })
+  .then(console.log('seeds created successfully'))
   .catch((err) => console.log(err))
-  for (resto of restaurants.results) {
-    Resto.create({
-      name: resto.name,
-      name_en: resto.name_en,
-      category: resto.category,
-      image: resto.image,
-      location: resto.location,
-      phone: resto.phone,
-      google_map: resto.google_map,
-      rating: resto.rating,
-      description: resto.description
-    })
-  }
 })
