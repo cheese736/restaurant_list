60582c8754c59e3196cc26009961a8037adc9d76
Aither <cheese736@hotmail.com>
2022-11-06 18:31:42 +0800

    make restaurant page personal

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,7 +3,8 @@ const router = express.Router()
 const Resto = require('../../models/restaurant')
 
 router.get('/', (req, res) => {
-  Resto.find()
+  const userId = req.user._id
+  Resto.find({userId})
     .lean()
     .then(restaurants => res.render('index', { restaurants }))
     .catch(error => console.log(error))
